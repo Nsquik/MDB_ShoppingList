ebb9bcffdbc065f06583f8fa6f4e838dc15927b0
Nsquik <46610139+Nsquik@users.noreply.github.com>
2020-07-18 03:04:06 +0200

    Add form functionality to add items to storage

--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,5 @@ import storageFactory from '@js/storage';
 
 const storage = storageFactory();
 storage.initializeCategories();
-storage.addItem('Owoce', {
-  item: 'gang',
-  quantity: 128,
-  quantity_type: 'kg',
-  category: 'owoce',
-  id: 30,
-});
-
-storage.editItem('Owoce', 30, {
-  item: 'Cola',
-  quantity: 15,
-  quantity_type: 'szt',
-  category: 'Napoje',
-  id: 32,
-});
 
 export default mdb;
